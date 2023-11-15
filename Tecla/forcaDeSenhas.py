5251e4e6ac83290eff5a80057b12454b09c3af2b
DanPeregrineDev <madeira.daf@gmail.com>
2023-11-15 12:04:21 +0000

    Add

---
+++ Tecla/forcaDeSenhas.py
@@ -0,0 +1,45 @@
+# Variables
+
+password = input("Escreva a password: ")
+
+rating = 0
+rating_length = 0
+rating_digit = 0
+rating_upper = 0
+rating_lower = 0
+rating_special = 0
+
+# Validation
+
+while password == "":
+    print("Password Inválida")
+    password = input("Escreva a password: ")
+
+# Process
+
+for letter in password:
+    if len(password) > 8:
+        rating_length = 1
+    if letter in "1234567890":
+        rating_digit = 1
+    if letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZÇ":
+        rating_upper = 1
+    if letter in "abcdefghijklmnopqrstuvwxyzç":
+        rating_lower = 1
+    if letter in "#&$@%!?;:.-_=+*<>\/^~[](){}":
+        rating_special = 1
+
+rating = rating_length + rating_digit + rating_upper + rating_lower + rating_special
+
+# Output
+
+if rating == 1:
+    print("Muito Fraca")
+elif rating == 2:
+    print("Fraca")
+elif rating == 3:
+    print("Média")
+elif rating == 4:
+    print("Forte")
+elif rating == 5:
+    print("Muito Forte")
