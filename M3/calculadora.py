f488588e4cb99113b2253fe848bf145da81fbbe0
DanPeregrineDev <madeira.daf@gmail.com>
2023-11-22 10:13:04 +0000

    Add

---
+++ M3/calculadora.py
@@ -0,0 +1,25 @@
+def Soma(P1, P2):
+    R = P1 + P2
+    print(R)
+
+def Subtracao(P1, P2):
+    R = P1 - P2
+    print(R)
+
+def Multiplicacao(P1, P2):
+    R = P1 * P2
+    print(R)
+
+def Divisao(P1, P2):
+    R = P1 / P2
+    print(R)
+
+print("1 - Somar")
+print("2 - Subtrair")
+print("3 - Multiplicar")
+print("4 - Dividir")
+
+Selecao = int(input(""))
+
+if Selecao == 1:
+    
--- M3/funcoes.py
+++ M3/funcoes.py
@@ -1,4 +1,11 @@
-def MinhaFunção():
-    print("Hello World")
+def SomaDeNumeros(P1, P2):
+    P = P1 + P2
+    print(f"A soma é {P}")
 
-MinhaFunção()
+SomaDeNumeros(4, 1)
+SomaDeNumeros(-4, 10)
+
+X1 = 10
+X2 = 40
+
+SomaDeNumeros(X1, X2)
