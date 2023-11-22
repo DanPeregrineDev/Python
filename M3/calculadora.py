7d4ea3151a2e5d697e74f5fcf24c6f5bb1a2b06f
DanPeregrineDev <madeira.daf@gmail.com>
2023-11-22 10:33:11 +0000

    Finished

--- M3/calculadora.py
+++ M3/calculadora.py
@@ -14,37 +14,51 @@ def Divisao(P1, P2):
     R = P1 / P2
     print(f"O resultado é {R}")
 
-print("1 - Somar")
-print("2 - Subtrair")
-print("3 - Multiplicar")
-print("4 - Dividir")
-print("0 - Fechar")
+# Process, Validation and Output
 
-# Variables
+while True:
+    print("1 - Somar")
+    print("2 - Subtrair")
+    print("3 - Multiplicar")
+    print("4 - Dividir")
+    print("0 - Fechar")
 
-Selecao = int(input("Selecione a opção: "))
-
-# Validation
-
-while Selecao < 1 or Selecao > 4:
-    print("Seleção inválida")
     Selecao = int(input("Selecione a opção: "))
 
-while Selecao != 0:
+    # Validation
+
+    while Selecao < 0 or Selecao > 4:
+        print("Seleção inválida")
+        Selecao = int(input("Selecione a opção: "))
+
     if Selecao == 1:
         V1 = int(input("Primeiro valor: "))
         V2 = int(input("Segundo valor: "))
+        print("")
         Soma(V1, V2)
+        print("")
+
     elif Selecao == 2:
         V1 = int(input("Primeiro valor: "))
         V2 = int(input("Segundo valor: "))
+        print("")
         Subtracao(V1, V2)
+        print("")
+
     elif Selecao == 3:
         V1 = int(input("Primeiro valor: "))
         V2 = int(input("Segundo valor: "))
+        print("")
         Multiplicacao(V1, V2)
+        print("")
+
     elif Selecao == 4:
         V1 = int(input("Primeiro valor: "))
         V2 = int(input("Segundo valor: "))
+        print("")
         Divisao(V1, V2)
-print("A terminar...")
+        print("")
+
+    elif Selecao == 0:
+        print("A terminar...")
+        break
