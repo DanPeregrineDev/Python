c1491001fb2a162e281859a7fb32ee4b19e2295f
DanPeregrineDev <madeira.daf@gmail.com>
2023-11-23 15:00:10 +0000

    add

--- M3/calculadora.py
+++ M3/calculadora.py
@@ -2,30 +2,33 @@
 
 def Soma(P1, P2):
     R = P1 + P2
-    print(f"O resultado é {R}")
+    return R
 
 def Subtracao(P1, P2):
     R = P1 - P2
-    print(f"O resultado é {R}")
+    return R
 
 def Multiplicacao(P1, P2):
     R = P1 * P2
-    print(f"O resultado é {R}")
+    return R
 
 def Divisao(P1, P2):
     R = P1 / P2
-    print(f"O resultado é {R}")
+    return R
 
-# Process, Validation and Output
-
-while True:
+def Menu():
     print("1 - Somar")
     print("2 - Subtrair")
     print("3 - Multiplicar")
     print("4 - Dividir")
     print("0 - Fechar")
-
     Selecao = int(input("Selecione a opção: "))
+    return Selecao
+
+# Process, Validation and Output
+
+while True:
+    Selecao = Menu()
 
     # Validation
 
@@ -39,28 +42,28 @@ while True:
         V1 = int(input("Primeiro valor: "))
         V2 = int(input("Segundo valor: "))
         print("")
-        Soma(V1, V2)
+        print(f"O resultado é: {Soma(V1, V2)}")
         print("")
 
     elif Selecao == 2:
         V1 = int(input("Primeiro valor: "))
         V2 = int(input("Segundo valor: "))
         print("")
-        Subtracao(V1, V2)
+        print(f"O resultado é {Subtracao(V1, V2)}")
         print("")
 
     elif Selecao == 3:
         V1 = int(input("Primeiro valor: "))
         V2 = int(input("Segundo valor: "))
         print("")
-        Multiplicacao(V1, V2)
+        print(f"O resultado é {Multiplicacao(V1, V2)}")
         print("")
 
     elif Selecao == 4:
         V1 = int(input("Primeiro valor: "))
         V2 = int(input("Segundo valor: "))
         print("")
-        Divisao(V1, V2)
+        print(f"O resultado é {Divisao(V1, V2)}")
         print("")
 
     elif Selecao == 0:
