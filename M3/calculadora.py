47024c84895872a2830e96d59fe564daa64a28ea
DanPeregrineDev <madeira.daf@gmail.com>
2023-11-27 09:41:14 +0000

    edit

--- M3/calculadora.py
+++ M3/calculadora.py
@@ -26,45 +26,49 @@ def Menu():
 
 # Process, Validation and Output
 
-while True:
-    Selecao = Menu()
+def main():
+    while True:
+        Selecao = Menu()
 
-    # Validation
+        # Validation
 
-    while Selecao < 0 or Selecao > 4:
-        print("Seleção inválida")
-        Selecao = int(input("Selecione a opção: "))
+        while Selecao < 0 or Selecao > 4:
+            print("Seleção inválida")
+            Selecao = int(input("Selecione a opção: "))
 
-    # Process and Output
+        # Process and Output
 
-    if Selecao == 1:
-        V1 = int(input("Primeiro valor: "))
-        V2 = int(input("Segundo valor: "))
-        print("")
-        print(f"O resultado é: {Soma(V1, V2)}")
-        print("")
+        if Selecao == 1:
+            V1 = int(input("Primeiro valor: "))
+            V2 = int(input("Segundo valor: "))
+            print("")
+            print(f"O resultado é: {Soma(V1, V2)}")
+            print("")
 
-    elif Selecao == 2:
-        V1 = int(input("Primeiro valor: "))
-        V2 = int(input("Segundo valor: "))
-        print("")
-        print(f"O resultado é {Subtracao(V1, V2)}")
-        print("")
+        elif Selecao == 2:
+            V1 = int(input("Primeiro valor: "))
+            V2 = int(input("Segundo valor: "))
+            print("")
+            print(f"O resultado é {Subtracao(V1, V2)}")
+            print("")
 
-    elif Selecao == 3:
-        V1 = int(input("Primeiro valor: "))
-        V2 = int(input("Segundo valor: "))
-        print("")
-        print(f"O resultado é {Multiplicacao(V1, V2)}")
-        print("")
+        elif Selecao == 3:
+            V1 = int(input("Primeiro valor: "))
+            V2 = int(input("Segundo valor: "))
+            print("")
+            print(f"O resultado é {Multiplicacao(V1, V2)}")
+            print("")
 
-    elif Selecao == 4:
-        V1 = int(input("Primeiro valor: "))
-        V2 = int(input("Segundo valor: "))
-        print("")
-        print(f"O resultado é {Divisao(V1, V2)}")
-        print("")
+        elif Selecao == 4:
+            V1 = int(input("Primeiro valor: "))
+            V2 = int(input("Segundo valor: "))
+            print("")
+            print(f"O resultado é {Divisao(V1, V2)}")
+            print("")
 
-    elif Selecao == 0:
-        print("A terminar...")
-        break
+        elif Selecao == 0:
+            print("A terminar...")
+            break
+
+if __name__ == "__main__":
+    main()
