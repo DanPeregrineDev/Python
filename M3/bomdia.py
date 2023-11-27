a5ab5d83468df6082d076178456d950ed93afe33
DanPeregrineDev <madeira.daf@gmail.com>
2023-11-27 10:35:03 +0000

    Edit

--- M3/bomdia.py
+++ M3/bomdia.py
@@ -1,7 +1,12 @@
+def PerguntarNome():
+    nome = input("Qual é o seu nome?: ")
+    return nome
+
 def Cumprimentar():
     print("Bom dia!")
 
 def main():
+    PerguntarNome()
     Cumprimentar()
 
 if __name__ == "__main__":
