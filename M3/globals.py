9043f4d8a150f2ce2b0360ea2aef670cd2ea42b8
DanPeregrineDev <madeira.daf@gmail.com>
2023-11-27 11:21:38 +0000

    Edit

--- M3/globals.py
+++ M3/globals.py
@@ -1,10 +1,21 @@
 Nome = "Joaquim"
+X = 0
+Y = 0
+R = 0
 
 def Cumprimentar():
     print(f"Bom dia {Nome}!")
 
+def Somar():
+    R = X + Y
+    X = 0
+
 def main():
     Cumprimentar()
+    Y = 5
+    X = 10
+    Somar()
+    print(R)
 
 if __name__ == "__main__":
     main()
