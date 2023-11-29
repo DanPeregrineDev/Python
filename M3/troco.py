b1ea8496bf23b143922ec16588bc8ad9144f31a4
DanPeregrineDev <madeira.daf@gmail.com>
2023-11-29 12:47:33 +0000

    add

--- M3/troco.py
+++ M3/troco.py
@@ -1,10 +1,16 @@
 def troco(pagar, dinheiro):
+    if dinheiro <= 0 or dinheiro < pagar:
+        print("Não tem dinheiro suficiente")
+        return None
     dinheiro = dinheiro - pagar
     print(f"Pagou {pagar} e ficou com {dinheiro}")
+    return dinheiro
 
 
 carteira = 100
+carteira = troco(50, carteira)
 
-
-troco(50, carteira)
-print(f"Tem na carteira {carteira}")
+if carteira != None:
+    print(f"Tem na carteira {carteira}")
+else:
+    print("Não tem dinheiro na carteira")
