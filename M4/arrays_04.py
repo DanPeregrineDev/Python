8c87b8736f2b95ac9a11472a294937663f85ba19
DanPeregrineDev <madeira.daf@gmail.com>
2024-01-10 12:08:09 +0000

    Fix

--- M4/arrays_04.py
+++ M4/arrays_04.py
@@ -2,7 +2,7 @@ import numpy as np
 
 VECTOR_MAX = 10
 
-numbers = np.array(VECTOR_MAX, dtype="f") # f - Floats
+numbers = np.empty(VECTOR_MAX, dtype="f") # f - Floats
 
 for i  in range(VECTOR_MAX):
     numbers[i] = int(input("Type a number: "))
