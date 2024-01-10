b7cff91a132dca0c9b443c666f5fa6c42e24d032
DanPeregrineDev <madeira.daf@gmail.com>
2024-01-10 13:06:47 +0000

    Add

---
+++ M4/MaisVelho.py
@@ -0,0 +1,20 @@
+"""
+    Ler o nome e idade de 5 pessoas e mostrar o mais velho
+"""
+
+import numpy as np
+
+N_PEOPLE = 5
+
+Nomes = np.empty(5, 'U20')
+Idades = np.empty(5, 'i')
+
+currentPerson = 0
+
+oldestPerson = Nomes[0]
+
+for i in range(N_PEOPLE):
+    Nomes[currentPerson] = input(f"Nome da {i}º pessoa: ")
+    Idades[currentPerson] = int(input(f"Idade da {i}º pessoa: "))
+
+    if 
