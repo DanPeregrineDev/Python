8643695b7977da94d3c5331edaf0434175ea7617
DanPeregrineDev <madeira.daf@gmail.com>
2024-02-08 14:48:30 +0000

    add

--- M5/tuples/ex13.py
+++ M5/tuples/ex13.py
@@ -1,6 +1,9 @@
 def media(tuplo):
     soma = 0
+    nPares = 0
     for i in tuplo:
-        soma += i
-    media = soma / len(tuplo)
+        if i % 2 == 0:
+            soma += i
+            nPares += 1
+    media = soma / nPares
     return media
