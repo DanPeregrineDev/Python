a3a76aa88cd1e285c7d74b5a43552c047b7f8a57
DanPeregrineDev <madeira.daf@gmail.com>
2024-02-21 12:09:01 +0000

    finished

--- M5/biblioteca/app.py
+++ M5/biblioteca/app.py
@@ -1,39 +1,107 @@
 import numpy as np
+from termcolor import colored, cprint
 
 # Função que permite ao utilizador adicionar um livro
 # O livro novo estará sempre disponivel
 # Verifique se ainda existe espaço no array antes de adicionar
 # Tenha atenção para não repetir o id do livro
-def adicionar_livro():
-    pass
+def adicionar_livro(biblioteca, nLivros):
+    titulo = input("Escreva o titulo do livro: ")
+
+    biblioteca[nLivros] = {"id_livro": nLivros + 1, "titulo": titulo, "disponivel": True, "nome_leitor":""}
+
+    cprint("Sucesso", "green")
+    return biblioteca
 
 
 # Função para mostrar os livros
 # deve permitir escolher se lista todos ou só os disponíveis ou só os indisponíveis
-def listar_livros():
-    pass
+def listar_livros(biblioteca, nLivros):
+
+    selection = 0
+
+    while selection < 1 or selection > 3:
+        print(f"Livros: {nLivros}")
+        print("1 - Mostrar todos")
+        print("2 - Mostrar disponíveis")
+        print("3 - Mostrar indesponíveis")
+        print("")
+
+        selection = int(input(""))
+        print("")
+
+        if selection < 1 or selection > 3:
+            cprint("Seleção inválida!", "red")
+            print("")
+
+    if selection == 1:
+        for livro in biblioteca:
+            if livro != None:
+
+                if livro['nome_leitor'] == "":
+                    livro['nome_leitor'] = "Nenhum"
+
+                print(f"ID: {livro['id_livro']} | Titulo: '{livro['titulo']}' | Disponível?: {livro['disponivel']} | Leitor: {livro['nome_leitor']}")
+                print("")
+
+    if selection == 2:
+        for livro in biblioteca:
+            if livro != None:
+                if livro['disponivel'] == True:
+
+                    if livro['nome_leitor'] == "":
+                        livro['nome_leitor'] = "Nenhum"
+
+                    print(f"ID: {livro['id_livro']} | Titulo: '{livro['titulo']}' | Disponível?: {livro['disponivel']} | Leitor: {livro['nome_leitor']}")
+                    print("")
+
+    if selection == 3:
+        for livro in biblioteca:
+            if livro != None:
+                if livro['disponivel'] == False:
+
+                    if livro['nome_leitor'] == "":
+                        livro['nome_leitor'] = "Nenhum"
+
+                    print(f"ID: {livro['id_livro']} | Titulo: '{livro['titulo']}' | Disponível?: {livro['disponivel']} | Leitor: {livro['nome_leitor']}")
+                    print("")
+
 
 
 # Função que muda o estado do livro e regista o nome do leitor
-def emprestar_livro():
-    pass
+def emprestar_livro(biblioteca):
+    idLivro = int(input("Escreva o id do livro: "))
+    nome = input("Escreva o nome do leitor: ")
+
+    for livro in biblioteca:
+        if livro != None:
+            if livro['id_livro'] == idLivro:
+                livro['disponivel'] = False
+                livro['nome_leitor'] = nome
+    
+    cprint("Sucesso", "green")
+    return
 
 
 #Função que torna o livro disponível e apaga o nome do leitor
 def devolver_livro(biblioteca):
-    idLivro = input("Qual o id do livro?: ")
+    idLivro = int(input("Qual o id do livro?: "))
 
-    for livros in biblioteca:
-        if biblioteca[livros[0]] == idLivro:
-            biblioteca[livros[2]] = True
-            biblioteca[livros[3]] = ""
+    for livro in biblioteca:
+        if livro != None:
+            if livro['id_livro'] == idLivro:
+                livro['disponivel'] = True
+                livro['nome_leitor'] = ""
 
-    print("Sucesso")
+    cprint("Sucesso", "green")
     return
 
 
 #Função que apresenta as opções ao utilizador e devolve a sua escolha
 def menu():
+
+    selecao = 0
+
     while selecao > 4 or selecao < 1:
         print("")
         print("1 - Adicionar livro")
@@ -43,9 +111,10 @@ def menu():
         print("")
     
         selecao = int(input(""))
+        print("")
 
         if selecao > 4 or selecao < 1:
-            print("Seleção inválida!")
+            cprint("Seleção inválida!", "red")
 
     return selecao
 
@@ -69,13 +138,14 @@ def main():
         selecao = menu()
 
         if selecao == 1:
-            pass
+            adicionar_livro(livros, nr_livros)
+            nr_livros += 1
 
         if selecao == 2:
-            pass
+            listar_livros(livros, nr_livros)
 
         if selecao == 3:
-            pass
+            emprestar_livro(livros)
 
         if selecao == 4:
             devolver_livro(livros)
