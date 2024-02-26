33d56c024bff47e274c888ab79272e146537edce
DanPeregrineDev <madeira.daf@gmail.com>
2024-02-26 10:30:45 +0000

    Add

--- M5/lists/2.py
+++ M5/lists/2.py
@@ -1,18 +1,15 @@
 benfica = []
 sporting = []
-nome = ""
-while nome != "Proximo":
-    nome = input(f"Escreva o nome do socio no clube do benfica (escreva proximo para continuar): ").capitalize()
-    if nome != "Proximo":
-        benfica.append(nome)
-nome = ""
-while nome != "Proximo":
-    nome = input(f"Escreva o nome do socio no clube do sporting (escreva proximo para continuar): ").capitalize()
-    if nome != "Proximo":
-        sporting.append(nome)
+def adicionarNome(a, b):
+    nome = ""
+    while nome != "Proximo":
+        nome = input(f"Escreva o nome do socio no clube do {b} (escreva proximo para continuar): ").capitalize()
+        if nome != "Proximo":
+            a.append(nome)
+adicionarNome(benfica, "Benfica")
+adicionarNome(sporting, "Sporting")
 for i in benfica:
     if i in sporting:
         benfica.remove(i)
         sporting.remove(i)
-
 print(benfica, sporting)
