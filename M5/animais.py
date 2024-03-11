487220661a492862abc377c8c020ec5c8057e624
DanPeregrineDev <madeira.daf@gmail.com>
2024-03-11 10:56:21 +0000

    Add

--- M5/animais.py
+++ M5/animais.py
@@ -1,18 +1,33 @@
-animais = {('Cao', 'Pitbull'): {'Idade': 5, 'Dono': 'Joaquim'}, ('Tartaruga', 'Ninja'): {'Idade': 10, 'Dono': 'Maria'}, ('Elefante', 'Voador'): {'Idade': 41, 'Dono': 'Pablo'}}
+animais = {("cão","pitbull"):{"idade":5,"dono":"joaquim"},
+           ("gato","persa"):{"idade":6,"dono":"joaquim"},
+           ("tartaruga","ninja"):{"idade":9,"dono":"ana"},
+           ("priquito","preto"):{"idade":18,"dono":"joaquim"},
+           ("priquito","pitbull"):{"idade":1,"dono":""},
+           ("cão","ninja"):{"idade":10,"dono":"ana"},
+           ("gato","preto"):{"idade":5,"dono":"joaquim"},
+}
 
 for chave, valor in animais.items():
-    if valor['Dono'] == "Joaquim":
+    if valor['dono'] == "Joaquim":
         print(chave[0], chave[1])
 
 print("")
 
-for chave, valor in animais.items():
-    print(chave[1])
+contagem = {}
+
+for chave in animais.keys():
+    especie = chave[0]
+    if especie in contagem:
+        contagem[especie] += 1
+    else:
+        contagem[especie] = 1
+print(contagem)
 
 print("")
 
+
 for i in animais:
     maior = 0
-    if animais[i]['Idade'] > maior:
-        maior = animais[i]['Idade']
+    if animais[i]['idade'] > maior:
+        maior = animais[i]['idade']
 print(f"O animal mais velho tem {maior} anos")
