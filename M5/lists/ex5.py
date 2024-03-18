72d9da1d0dc1ace791c976e9162a6cd5ea695eb9
DanPeregrineDev <madeira.daf@gmail.com>
2024-03-18 10:43:30 +0000

    Add

--- M5/lists/ex5.py
+++ M5/lists/ex5.py
@@ -1,26 +1,8 @@
-anoDoUser = int(input("Escreva o ano em que nasceu: "))
+signos = ["Rato", "Búfalo", "Tigre", "Coelho", "Dragão", "Serpente", "Cavalo", "Cabra", "Macaco", "Galo", "Cão", "Porco"]
 
-if anoDoUser % 12 == 0:
-    print("Rato")
-elif anoDoUser % 12 + 1 == 0:
-    print("Bufalo")
-elif anoDoUser % 12 + 2 == 0:
-    print("Tigre")
-elif anoDoUser % 12 + 3 == 0:
-    print("Coelho")
-elif anoDoUser % 12 + 4 == 0:
-    print("Dragao")
-elif anoDoUser % 12 + 5 == 0:
-    print("Serpente")
-elif anoDoUser % 12 + 6 == 0:
-    print("Cavalo")
-elif anoDoUser % 12 + 7 == 0:
-    print("Cabra")
-elif anoDoUser % 12 + 8 == 0:
-    print("Macaco")
-elif anoDoUser % 12 + 9 == 0:
-    print("Galo")
-elif anoDoUser % 12 + 10 == 0:
-    print("Cachorro")
-elif anoDoUser % 12 + 11 == 0:
-    print("Porco")
+data_nascimento = input("Data de nascimento (dd/mm/yyyy): ")
+ano = int(data_nascimento[6:])
+
+posicao = (ano - 1900) % 12
+
+print(signos[posicao])
