5326a2084a90cb439cfc35839adac6a692b6fa47
DanPeregrineDev <madeira.daf@gmail.com>
2024-03-20 10:20:11 +0000

    Add

--- M5/produto.py
+++ M5/produto.py
@@ -1,1 +1,9 @@
-pingoDoce = {"Arroz": 1.18, "Maçã": 2.85, "Leite": 0.82, "Frango": 4.98, "Café": 36.98, "Água": 1.29, "Azeite": 9.90, "Sal Grosso": 0.25, "Açucar": 7.30, "Salmão": 12.99}
+# Programa para sugerir a loja onde um cabaz de produtos é mais barato
+# O utilizador insere um cabaz de produtos a comprar
+# Se não existirem todos os produtos a comprar deve informar o utilizador
+
+pingoDoce = {"Arroz": 1.18, "Maçã": 2.85, "Leite": 0.82, "Frango": 4.98, "Café": 22.9, "Água5L": 1.29, "Azeite": 9.90, "Sal Grosso": 0.25, "Açucar": 7.30, "Salmão": 12.99}
+
+auchan = {"Frango":2.34,"Arroz":1.05,"Maçã":1.59,"Leite":0.80,"Azeite":12.68,"Água5L":1.05,"Água6L":0.84,"Sal Grosso":2.29,"Salmão":24.93,"Açucar":1.79,"Café":6.59}
+
+continente = {"loja":{"Arroz":1.83,"Maçã":1.99,"Leite":0.82,"Frango":1.25,"Café":6.59,"Água5L":1.05,"Água6L":0.84}}
