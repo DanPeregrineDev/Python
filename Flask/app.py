bc0f1e12e657434ea5c3777e90f45ccede0ee260
DanPeregrineDev <madeira.daf@gmail.com>
2024-03-21 14:56:24 +0000

    Add

--- Flask/app.py
+++ Flask/app.py
@@ -4,6 +4,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello world!"
+    return render_template("index.html")
+
+@app.route("/somar", methods=["POST"])
+def somar():
+    if request.form.get("n1") == "" or request.form.get("n2") == "":
+        return render_template("index.html", resultado = "Tem de preencher todos os campos")
+
+
+    n1 = int(request.form.get("n1"))
+    n2 = int(request.form.get("n2"))
+    soma = n1 + n2
+    return render_template("index.html", resultado = soma)
+
 
 app.run(debug=True)
